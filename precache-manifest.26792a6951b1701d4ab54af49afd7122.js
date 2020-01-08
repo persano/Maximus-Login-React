6ca991cd702a431fa622fcba060b742cc9634e12
persano <santiagopostorivo@gmail.com>
2020-01-08 11:54:16 -0300

    Updates

---
+++ precache-manifest.26792a6951b1701d4ab54af49afd7122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b18a4dcf87d1404b0b753fc73354a8",
+    "url": "/Maximus-Login-React/index.html"
+  },
+  {
+    "revision": "3ae3af41499dddffc032",
+    "url": "/Maximus-Login-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a0dacde27188f58993e",
+    "url": "/Maximus-Login-React/static/js/2.9a60b027.chunk.js"
+  },
+  {
+    "revision": "9fc635aceec4ff3bb823043ab022e718",
+    "url": "/Maximus-Login-React/static/js/2.9a60b027.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ae3af41499dddffc032",
+    "url": "/Maximus-Login-React/static/js/main.fccb1978.chunk.js"
+  },
+  {
+    "revision": "bead94daa8afbf1055ea",
+    "url": "/Maximus-Login-React/static/js/runtime-main.a7880da6.js"
+  }
+]);
